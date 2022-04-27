0a17a2641353cfc238c15a2444ae0aaaa6e4bdb9
tusharshastrakar <101271034+tusharshastrakar@users.noreply.github.com>
2022-04-27 15:40:59 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,18 @@
+'''def login():#calling function
+    while True:
+        username = input("enter your username:")
+        password = input("enter your password:")
+        if username == password:
+            print("login succesfully")
+            break
+        else:
+            print("wrong information plz try again")
+
+login()'''
+'''import time
+name = "good night sweat dreams"
+for i in name:
+    time.sleep(1)
+    print(i,end=" ")'''
+
+
